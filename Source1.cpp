a547a94949e0def5e29c8fb91ad29cc5a8957def
sabegomaa261 <sabergomaa261@gmail.com>
2022-12-20 15:37:25 +0200

    F

--- Source1.cpp
+++ Source1.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[]) {
 	printf("Enter Marks Number \n");
 	scanf_s("%d", &sz);
 
-	x = (double*)malloc(sz * sizeof(double));
+	x = (double*)malloc(sz* sizeof(double));
 	y = (double*)malloc(sz * sizeof(double));
 
 
